a5d0946c2469eb3d098c234bbaec295b258efc4c
unkrunner <unk.runner@yandex.ua>
2014-06-06 08:56:09 +0300

    made changes according to received comments

--- generator.py
+++ generator.py
@@ -1,44 +1,88 @@
 from yaml import load, YAMLError
 
 
-def load_data(filename):
-    try:
-        with open(filename, 'r') as source:
-            dump_data = load(source)
-            if dump_data is None:
-                print("File is empty. Nothing to parse")
-            source.close()
-            return dump_data
-    except FileNotFoundError:
-        print("No such file or directory: {}".format(filename))
-        return False
-    except YAMLError:
-        print("Could not parse file: {}".format(filename))
-        source.close()
-        return False
+class Generator(object):
+    __create_table_string = 'CREATE TABLE "{}"('
+    __serial_string       = '\n\t{}_id SERIAL NOT NULL, '
+    __created_string      = '{}_created timestamp NOT NULL DEFAULT now(),'
+    __updated_string      = '{}_updated timestamp NOT NULL DEFAULT now()'
 
 
-def generate_ddl_statements(in_file, out_file):
-    dump_data = load_data(in_file)
-    if dump_data:
-        result = ''
-        for (table_name, properties) in dump_data.items():
-            table_name = table_name.lower()
-            result = '\n'.join([result, 'CREATE TABLE "{}"('.format(table_name)])
-            for (property_name, fields) in properties.items():
-                if property_name == 'fields':
-                    result = '\t'.join([result, '\n\t{}_id SERIAL NOT NULL, '.format(table_name)])
-                    for (field, value) in fields.items():
-                        result = '\n\t'.join([result, '{}_{} {}, '.format(table_name, field, value.upper())])
-            result = '\n\t'.join([result, '{}_created timestamp NOT NULL DEFAULT now(),'.format(table_name), \
-                                  '{}_updated timestamp NOT NULL DEFAULT now()\n);'.format(table_name),])
-        print(result)
+
+    def __init__(self):
+        self._alters   = set()
+        self._tables   = set()
+        self._triggers = set()
+        self._schema = ''
+
+    def __build_tables(self):
+        for (entity) in self._schema.keys():
+            table_statement= self.__create_table_string.format(entity.lower())
+            self._tables.add('{} {});\n'.format(table_statement, self.__build_columns(entity)))
+
+    def __build_columns(self, entity):
+        table_name = entity.lower()
+        field_statement = self.__serial_string.format(table_name)
+        for (field, value) in self._schema[entity]['fields'].items():
+            field_statement = '\n\t'.join([field_statement, '{}_{} {}, '
+                .format(table_name, field, value.upper())])
+        field_statement = '\n\t'.join([field_statement, self.__created_string.
+            format(table_name),self.__updated_string.format(table_name),])
+        return field_statement
+
+    def __build_relations(self):
+        pass
+
+    def __build_many_to_one(self, child, parent):
+        pass
+
+    def __build_many_to_many(self, left, right):
+        pass
+
+    def __build_triggers(self):
+        pass
+
+    def build_ddl(self, filename):
+        #parse yaml schema and fill tables, alters and triggers
+        self._schema = self.__class__.load_data(filename)
+        if self._schema:
+            self.__build_tables()
+
+    def clear(self):
+        #clear tables, alters and triggers
+        self._alters    = set()
+        self._tables    = set()
+        self._triggers  = set()
+        self._schema = ''
+
+
+    def dump(self, filename):
+        #write tables, then alters and triggers to file
         try:
-            f = open(out_file, 'w')
-            f.write(result)
+            f = open(filename, 'w')
+            for table in self._tables:
+                f.write(str(table))
             f.close()
         except:
             print('Unable to write to file')
 
+    @staticmethod
+    def load_data(filename):
+        try:
+            with open(filename, 'r') as source:
+                schema = load(source)
+                if schema is None:
+                    print("File is empty. Nothing to parse")
+                source.close()
+                return schema
+        except FileNotFoundError:
+            print("No such file or directory: {}".format(filename))
+            return False
+        except YAMLError:
+            print("Could not parse file: {}".format(filename))
+            return False
+
 if __name__ == '__main__':
-    generate_ddl_statements('test.yaml', 'statements.sql')
+    generator = Generator()
+    generator.build_ddl('test.yaml')
+    generator.dump('statements.sql')
