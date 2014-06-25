d3ca475700a44c70ec86aa4a7ed35e18e0c0391c
unkrunner <unk.runner@yandex.ua>
2014-06-25 13:00:45 +0300

    generator updated according to x-treme comments 	for (entity) — nice! нахрена тут скобки? 	форматирование запросов в отдельные переменные

--- generator.py
+++ generator.py
@@ -7,27 +7,24 @@ class Generator(object):
     {columns}
     {table}_created timestamp NOT NULL DEFAULT now(),
     {table}_updated timestamp NOT NULL DEFAULT now(),
-    PRIMARY KEY ({table}_id)
-);\n"""
+    PRIMARY KEY ({table}_id)\n);\n"""
 
     __create_join_table = """CREATE TABLE "{left}__{right}" (
     "{left}_id" integer NOT NULL,
     "{right}_id" integer NOT NULL,
-    PRIMARY KEY ({left}_id, {right}_id)
-);\n"""
+    PRIMARY KEY ({left}_id, {right}_id)\n);\n"""
+
     __alter_string = """\nALTER TABLE "{child}" ADD "{parent}_id" integer NOT NULL,
     ADD CONSTRAINT "fk_{child}_{parent}_id" FOREIGN KEY ("{parent}_id")
     REFERENCES "{parent}" ("{parent}_id");\n"""
 
-    __trigger_string = """\nCREATE OR REPLACE FUNCTION update_{table}_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.{table}_updated = now();
-    RETURN NEW;
-END;
-$$ language 'plpgsql';
-CREATE TRIGGER "tr_{table}_updated" BEFORE UPDATE ON "{table}" FOR EACH ROW EXECUTE\n\
-PROCEDURE update_{table}_timestamp();\n"""
+    __trigger_proc_head = """\nCREATE OR REPLACE FUNCTION update_{table}_timestamp()\nRETURNS TRIGGER AS $$\n"""
+
+    __trigger_proc_body = "BEGIN\n\tNEW.{table}_updated = now();\n\tRETURN NEW;\nEND;\n"
+
+    __create_trigger_str = """$$ language 'plpgsql';\nCREATE TRIGGER "tr_{table}_updated"
+    BEFORE UPDATE ON "{table}" FOR EACH ROW EXECUTE PROCEDURE update_{table}_timestamp();\n"""
+
 
     def __init__(self):
         self._alters   = set()
@@ -36,8 +33,9 @@ PROCEDURE update_{table}_timestamp();\n"""
         self._schema = ''
 
     def __build_tables(self):
-        for (entity) in self._schema.keys():
-            self._tables.add(self.__create_table_string.format(table=entity.lower(), columns=self.__build_columns(entity)))
+        for entity in self._schema.keys():
+            self._tables.add(self.__create_table_string.format(table=entity.lower(),
+                                                               columns=self.__build_columns(entity)))
 
     def __build_columns(self, entity):
         for (field, value) in self._schema[entity]['fields'].items():
@@ -46,22 +44,18 @@ PROCEDURE update_{table}_timestamp();\n"""
 
     def __build_relations(self):
         def _order_tables(table1, table2):
-            string1 = table1.lower()
-            string2 = table2.lower()
-            if string1[0] < string2[0]:
-                return {'left': string1, 'right': string2}
-            return {'left': string2, 'right': string1}
+            return tuple(sorted([table1.lower(), table2.lower()]))
 
         for entity in self._schema.keys():
             for (table_name, relation_type) in self._schema[entity]['relations'].items():
-                if relation_type == 'one':
-                    self.__build_many_to_one(entity.lower(), table_name.lower())
-                if relation_type == 'many':
-                    try:
-                        if self._schema[table_name]['relations'][entity] == 'many':
-                            self.__build_many_to_many(**_order_tables(entity.lower(), table_name.lower()))
-                    except KeyError:
-                        pass
+                try:
+                    if relation_type == 'one' and self._schema[table_name]['relations'][entity] == 'many':
+                        self.__build_many_to_one(entity.lower(), table_name.lower())
+                    if relation_type == 'many' and self._schema[table_name]['relations'][entity] == 'many':
+                        self.__build_many_to_many(*_order_tables(entity.lower(), table_name.lower()))
+                except KeyError:
+                    print('Bad schema. Could not find correct relations between {} '
+                          'and {}'.format(table_name, entity))
 
     def __build_many_to_one(self, child, parent):
         self._alters.add(self.__alter_string.format(child=child, parent=parent))
@@ -71,7 +65,8 @@ PROCEDURE update_{table}_timestamp();\n"""
 
     def __build_triggers(self):
         for entity in self._schema.keys():
-            self._triggers.add(self.__trigger_string.format(table=entity.lower()))
+            trigger_string = ''.join([self.__trigger_proc_head, self.__trigger_proc_body, self.__create_trigger_str])
+            self._triggers.add(trigger_string.format(table=entity.lower()))
 
     def build_ddl(self, filename):
         #parse yaml schema and fill tables, alters and triggers
@@ -92,13 +87,9 @@ PROCEDURE update_{table}_timestamp();\n"""
         #write tables, then alters and triggers to file
         try:
             f = open(filename, 'w')
-            for table in self._tables:
-                f.write(str(table))
-            for alter in self._alters:
-                f.write(str(alter))
-            for trigger in self._triggers:
-                f.write(str(trigger))
-
+            f.write('\n'.join([table for table in self._tables]))
+            f.write(''.join([alter for alter in self._alters]))
+            f.write(''.join([trigger for trigger in self._triggers]))
             f.close()
         except IOError:
             print('Unable to write to file')
